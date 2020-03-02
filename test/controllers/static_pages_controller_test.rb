caba1f98e5e1863e96e798f29d24b7fac15c72dc
conejitomaya <eduardobonillac@gmail.com>
2020-03-02 10:20:22 -0600

    staticpages controller generated added custom.scss
    staticpages > home help login signup

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get login" do
+    get static_pages_login_url
+    assert_response :success
+  end
+
+  test "should get signup" do
+    get static_pages_signup_url
+    assert_response :success
+  end
+
+end
