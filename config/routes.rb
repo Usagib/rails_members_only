4beb7377aa33c984a8cdfbce0861bd111d91d5f1
conejitomaya <eduardobonillac@gmail.com>
2020-03-02 14:01:43 -0600

    session controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'session/new'
+  get 'session/create'
+  get 'session/destroy'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
-  get '/login', to: 'static_pages#login'
-  get '/signup', to: 'static_pages#signup'
+  get '/login', to: 'session#new'
+  get '/signup', to: 'user#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
