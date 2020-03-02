4a2f19e25d28ec6c2ea05ee16613a8f6e0b3af18
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-03-02 11:41:17 -0500

    ran basic db migrate
    Added user constraints and ran migrate to db schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
+# User model specifications
 class User < ApplicationRecord
+  # formats the email to down case before saving
+  before_save { self.email = email.downcase }
+
+  # Allows email validations for correct format
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 255 }
+  validates :email, presence: true, length: { minimum: 10, maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
