972e3ce3b18b215ce31f516a513ca538bcd39eea
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-03-02 17:54:22 -0500

    Added authentication skeleton for session
    replaced the bcrypt password method for a SHA1 digest hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 # User model specifications
 class User < ApplicationRecord
   # formats the email to down case before saving
+  attr_accessor :remember_token
+  before_create :create_activation_digest
   before_save { self.email = email.downcase }
 
   # Allows email validations for correct format
@@ -16,4 +18,28 @@ class User < ApplicationRecord
 
   has_secure_password
   has_many :posts
+
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      Digest::SHA1.hexdigest string.to_s
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    Digest::SHA1.hexdigest(remember_digest).is_password?(remember_token)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_222556) do
+ActiveRecord::Schema.define(version: 2020_03_02_224821) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_222556) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "remember_token"
+    t.string "remember_digest"
   end
 
 end
