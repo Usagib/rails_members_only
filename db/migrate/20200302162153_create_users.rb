31321d788ba6f9ca2309527676d1b207848c0ee6
conejitomaya <eduardobonillac@gmail.com>
2020-03-02 10:23:28 -0600

    AddModels
    User andPost model added

---
+++ db/migrate/20200302162153_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.integer :post_id
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
