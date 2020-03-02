171ce2d67014566dcbd9d223a8ed0009c49d886e
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-03-02 16:24:57 -0500

    Fixed patch method for user form
    users edit and new path are now working

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create' # helps us cookies creation
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create' # helps us cookies creation
+  post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users, except: %i[new]
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
